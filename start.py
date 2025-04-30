65363b63776b93e4b1d8198ef76b6db8c7e36615
Faith Maina <88420310+Mainadeveloper@users.noreply.github.com>
2025-04-30 06:47:30 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 import time
 import binalert
 
-# Run forever, checking every 6 hours
 while True:
     print(binalert.run_alert())
-    time.sleep(21600)  # 6 hours
+    time.sleep(300)  # 5 minutes
